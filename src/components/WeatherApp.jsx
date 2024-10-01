1dcd7bd813bd3c21ca5271fa532c17edb52f519e
Dave Abhishek <abhishekjdave0309@gmail.com>
2024-10-01 14:47:28 +0530

    current location feature added in weather app

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -31,15 +31,40 @@ function WeatherApp() {
       .catch((error) => {
         console.log("Data Fetch Error:", error);
         setError(error.message);
-        if(location == ""){
-          setLocation("Ahmedabad")
+        if (location == "") {
+          setLocation("Ahmedabad");
+        }
+      });
+  };
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        fetchWeatherByCoords(latitude, longitude);
+      });
+    };
+  });
+
+  const fetchWeatherByCoords = (lat, lon) => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${
+        import.meta.env.VITE_OPENWEATHER_API_KEY
+      }`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setWeatherData(data);
+        setLocation(data.name);
+        if (data.weather && data.weather[0] && data.weather[0].icon) {
+          getWeatherIcon(data.weather[0].icon);
         }
       });
   };
 
   const handlechange = (e) => {
     setLocation(e.target.value);
-    if(location == ''){
+    if (location == "") {
       setLocation("ahmedabad");
     }
   };
@@ -76,62 +101,60 @@ function WeatherApp() {
             {icon && <img className="h-[22vw]" src={icon} alt="Weather icon" />}
           </div>
           <div className="weather-info w-1/2 h-full flex flex-col justify-center gap-10">
-                {error ? (
-                  <div className="error-message text-red-500 text-center text-[2vw]">
-                    {error}
-                  </div>
-                ) : weatherData ? (
-                  <>
-                    <div className="temp-wrapper flex flex-col justify-center items-center">
-                      <span className="text-[5vw]">
-                        {weatherData.main && weatherData.main.temp
-                          ? Math.round(weatherData.main.temp)
+            {error ? (
+              <div className="error-message text-red-500 text-center text-[2vw]">
+                {error}
+              </div>
+            ) : weatherData ? (
+              <>
+                <div className="temp-wrapper flex flex-col justify-center items-center">
+                  <span className="text-[5vw]">
+                    {weatherData.main && weatherData.main.temp
+                      ? Math.round(weatherData.main.temp)
+                      : "Location Not Found"}
+                    <sup>°</sup>C
+                  </span>
+                  <span className="text-[3vw]">
+                    {weatherData.name || "Location not found"}
+                  </span>
+                </div>
+                <div className="w-full flex justify-center gap-10">
+                  <div className="Humidity-wrapper flex flex-col">
+                    <div className="inner-humidity-wrapper flex">
+                      <img
+                        className="w-10"
+                        src="https://img.icons8.com/?size=100&id=sjlgpDZO1OtH&format=png&color=000000"
+                        alt="Humidity icon"
+                      />
+                      <span className="text-[2vw]">
+                        {weatherData.main && weatherData.main.humidity
+                          ? `${weatherData.main.humidity}%`
                           : "Location Not Found"}
-                        <sup>°</sup>C
-                      </span>
-                      <span className="text-[3vw]">
-                        {weatherData.name || "Location not found"}
                       </span>
                     </div>
-                    <div className="w-full flex justify-center gap-10">
-                      <div className="Humidity-wrapper flex flex-col">
-                        <div className="inner-humidity-wrapper flex">
-                          <img
-                            className="w-10"
-                            src="https://img.icons8.com/?size=100&id=sjlgpDZO1OtH&format=png&color=000000"
-                            alt="Humidity icon"
-                          />
-                          <span className="text-[2vw]">
-                            {weatherData.main && weatherData.main.humidity
-                              ? `${weatherData.main.humidity}%`
-                              : "Location Not Found"}
-                          </span>
-                        </div>
-                        <div className="heading text-[1.5vw]">Humidity</div>
-                      </div>
-
-                      <div className="Humidity-wrapper flex flex-col">
-                        <div className="inner-humidity-wrapper flex">
-                          <img
-                            className="w-10"
-                            src="https://img.icons8.com/?size=100&id=74197&format=png&color=000000"
-                            alt="Wind speed icon"
-                          />
-                          <span className="text-[2vw]">
-                            {weatherData.wind && weatherData.wind.speed
-                              ? `${weatherData.wind.speed}km/h`
-                              : "Location Not Found"}
-                          </span>
-                        </div>
-                        <div className="heading text-[1.5vw]">Wind Speed</div>
-                      </div>
-                    </div>
-                  </>
-                ) : (
-                  <div className="loading text-center text-[2vw]">
-                    Loading...
+                    <div className="heading text-[1.5vw]">Humidity</div>
                   </div>
-                )}
+
+                  <div className="Humidity-wrapper flex flex-col">
+                    <div className="inner-humidity-wrapper flex">
+                      <img
+                        className="w-10"
+                        src="https://img.icons8.com/?size=100&id=74197&format=png&color=000000"
+                        alt="Wind speed icon"
+                      />
+                      <span className="text-[2vw]">
+                        {weatherData.wind && weatherData.wind.speed
+                          ? `${weatherData.wind.speed}km/h`
+                          : "Location Not Found"}
+                      </span>
+                    </div>
+                    <div className="heading text-[1.5vw]">Wind Speed</div>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <div className="loading text-center text-[2vw]">Loading...</div>
+            )}
 
             <div className="search-bar w-full flex justify-center items-center mt-5">
               <input
